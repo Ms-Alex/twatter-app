c37f55132eccf2386b0a6e6e31e3007f862401d0
Ms-Alex <ms.hernandeza1@gmail.com>
2019-02-26 21:12:48 -0500

    add user seed file

--- seed.js
+++ seed.js
@@ -1,3 +1,5 @@
+require("dotenv").load();
+
 module.exports = {
 	"undefined": process.env.MONGODB_URI || "mongodb://localhost/twatter",
 	"dev": process.env.MONGODB_URI || "mongodb://localhost/twatter",
