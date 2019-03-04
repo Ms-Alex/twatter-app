687e1435d258b6a8431674f197306ff6915eda7c
Ms-Alex <ms.hernandeza1@gmail.com>
2019-03-03 19:10:13 -0500

    add different seed library, and create seed file

--- seed.js
+++ seed.js
@@ -1,7 +1,36 @@
-require("dotenv").load();
+const seeder = require('mongoose-seed');
 
-module.exports = {
-	"undefined": process.env.MONGODB_URI || "mongodb://localhost/twatter",
-	"dev": process.env.MONGODB_URI || "mongodb://localhost/twatter",
-	"prod": process.env.MONGODB_URI || "mongodb://localhost/twatter"
-}
+// Connect to MongoDB via Mongoose
+seeder.connect(process.env.MONGODB_URI || "mongodb://localhost/twatter", {
+    // To keep connection open
+    keepAlive: true,
+    useNewUrlParser: true
+}, function () {
+
+    // Load Mongoose models
+    seeder.loadModels([
+        'models/user.js'
+    ]);
+
+    // Clear specified collections
+    seeder.clearModels(['User'], function () {
+
+        // Callback to populate DB once collections have been cleared
+        seeder.populateModels(data, function () {
+            seeder.disconnect();
+        });
+
+    });
+});
+
+// Data array containing seed data - documents organized by Model
+let data = [{
+    'model': 'User',
+    'documents': [{
+        'email': 'user1@email.com',
+        'username': 'user_1',
+        'password': 'user123',
+        'profileImageUrl': 'https://d2lv662meabn0u.cloudfront.net/show/00000000/9/829e96a3699d831e342f899b139e18cb58ebe0c5_1538660377.jpg'
+    }
+    ]
+}];
